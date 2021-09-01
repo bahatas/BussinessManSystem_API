42e6e582a4dcab62e425a93e73b78a9d67aab68a
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-09-01 15:43:45 +0100

    Initial commit of Business Management System

---
+++ src/main/java/com/example/bussinessmansystem_api/BussinessManSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bussinessmansystem_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinessManSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BussinessManSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bussinessmansystem_api/BussinessManSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bussinessmansystem_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinessManSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
