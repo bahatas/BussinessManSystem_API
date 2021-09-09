3c87e7cd101d1160f5fec844ab2886b6fc216238
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-09-09 10:00:51 +0100

    DefaultExceptionmessageDTO

---
+++ src/main/java/com/cybertek/businessmansystem_api/dto/DefaultExceptionMessageDTO.java
@@ -0,0 +1,13 @@
+package com.cybertek.businessmansystem_api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class DefaultExceptionMessageDTO {
+
+    private String message;
+}
